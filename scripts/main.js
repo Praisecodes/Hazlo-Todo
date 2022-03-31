d061c7d95b94a9e724bc0fec176b794c6e6bb157
Praisecodes <cmancoder@gmail.com>
2022-03-31 17:48:44 +0100

    Cool Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
-console.log("Be Organized");
+const LoginBtn = document.querySelector(".login");
+const SignUpBtn = document.querySelector(".signUp");
+
+let typer = new Typed("#catchPhrases", {
+    strings:[
+        "ORGANIZED",
+        "ON TIME",
+        "PRODUCTIVE",
+        "CONSERVATIVE"
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
