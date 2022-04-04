48451301fb2a1b6e74d8bb37da47483216bc36c9
Praisecodes <cmancoder@gmail.com>
2022-04-04 23:50:35 +0100

    Did some crazy shit lmao

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+    if(isset($_SESSION["username"])){
+        header("Location: workarea/");
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- workarea/index.php
+++ workarea/index.php
@@ -1,4 +1,8 @@
-<?php session_start(); ?>
+<?php session_start(); 
+    if(!isset($_SESSION["username"])){
+        header("Location: ../");
+    }
+?>
 
 <!DOCTYPE html>
 <html lang="en">
