6895cce95e494faed1b026645ad0f3bab5c775e6
Praisecodes <cmancoder@gmail.com>
2022-04-09 22:54:39 +0100

    ..

--- workarea/signout.php
+++ workarea/signout.php
@@ -2,7 +2,7 @@
     session_start();
     setcookie("usersname", "", time()-1, "/");
     setcookie("fullname", "", time()-1, "/");
-    $_SESSION["userFname"] = null;
-    $_SESSION["username"] = null;
+    $_SESSION['userFname'] = null;
+    $_SESSION['username'] = null;
     header("Location: ../");
 ?>
