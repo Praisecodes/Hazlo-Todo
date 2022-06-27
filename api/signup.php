3353ef43d4a39de3f46e4869b10af1271b743cc9
Praisecodes <cmancoder@gmail.com>
2022-06-27 03:08:15 +0100

    Update: Work On Signup BackEnd

---
+++ api/signup.php
@@ -0,0 +1,87 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Headers: Content-Type");
+
+    require_once "connection.php";
+
+    $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : "Not Set";
+
+    $fullname = null; $email = null; $username = null; $password = null; $confirm_password = null;
+
+    if($contentType === "application/json"){
+        $contents = trim(file_get_contents("php://input"));
+
+        $decoded = json_decode($contents, true);
+        
+        if($decoded["Confirm_password"] === $decoded["Password"]){
+            $password = TestInput($decoded["Confirm_password"]);
+            $fullname = TestInput($decoded["Fullname"]);
+            $username = TestInput($decoded["Username"]);
+            $email = TestInput($decoded["Email"]);
+
+            $check_sql = "SELECT * FROM user_info WHERE username=?";
+            $check_stmt = $conn->prepare($check_sql);
+            $check_stmt->bind_param('s', $username);
+            if(!($check_stmt->execute())){
+                echo json_encode([
+                    "Error 1x01: Fatal Execution Error"
+                ]);
+                $check_stmt->close();
+                $conn->close();
+                http_response_code(500);
+                exit;
+            }
+            else{
+                $result = $check_stmt->get_result();
+                if($result->num_rows > 0){
+                    echo json_encode([
+                        "This username already exists"
+                    ]);
+                    $check_stmt->close();
+                    $conn->close();
+                    http_response_code(200);
+                    exit;
+                }
+                else{
+                    $Sql = "INSERT INTO user_info(fullname, username, user_password, email) VALUES (?,?,?,?);";
+                    $stmt = $conn->prepare($Sql);
+                    $stmt->bind_param('ssss', $fullname, $username, $password, $email);
+                    if(!($stmt->execute())){
+                        echo json_encode([
+                            "Error 1x02: Fatal Execution Error"
+                        ]);
+                        $stmt->close();
+                        $conn->close();
+                        http_response_code(500);
+                        exit;
+                    }
+                    else{
+                        echo json_encode([
+                            "Success"
+                        ]);
+                        http_response_code(200);
+                        $stmt->close();
+                        $conn->close();
+                        exit;
+                    }
+                }
+            }
+        }
+        else{
+            echo json_encode([
+                "Password Mismatch"
+            ]);
+            exit;
+        }
+        
+    }
+
+    function TestInput($data){
+        $data = stripslashes($data);
+        $data = trim($data);
+        $data = htmlspecialchars($data);
+
+        return $data;
+    }
+?>
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,1 +1,34 @@
-console.log("Signup");
+const Username = document.querySelector(".Username");
+const Fullname = document.querySelector(".Fullname");
+const Email = document.querySelector(".Email");
+const Password = document.querySelector(".Password");
+const CPassword = document.querySelector(".CPassword");
+const signup = document.querySelector(".signup");
+
+
+signup.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    let Details = {
+        "Fullname": Fullname.value,
+        "Username": Username.value,
+        "Email": Email.value,
+        "Password": Password.value,
+        "Confirm_password": CPassword.value
+    };
+
+    fetch("../api/signup.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(Details)
+    })
+    .then(res=>res.json())
+    .then((data)=>{
+        console.log(data);
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+})
