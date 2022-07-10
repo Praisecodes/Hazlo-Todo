21a77060b3cfeeb15176849aebbf97eb8ce49b2d
Praisecodes <cmancoder@gmail.com>
2022-07-10 14:09:04 +0100

    Update: Working on workarea

---
+++ scripts/workarea.js
@@ -0,0 +1,26 @@
+// const workarea_nav = document.querySelector('.workarea_nav');
+// const topperIcons = document.querySelectorAll('.topperIcons');
+// const topper = document.querySelector('.workarea_topper');
+// const switchMode = document.querySelector('.switchMode');
+// const icon_dropdown = document.querySelector('.icon_dropdown');
+
+// switchMode.innerHTML = `<i class="fa fa-moon"></i>`;
+// document.body.style.backgroundColor = "var(--mainColor)";
+// workarea_nav.style.backgroundColor = "var(--darkModeLight)";
+// topper.style.borderBottom = "1px solid var(--darkModeLight)";
+// document.body.style.color = "white";
+// icon_dropdown.addEventListener('mouseover', ()=>{
+//     icon_dropdown.style.backgroundColor = "var(--darkModeLight)";
+// });
+// icon_dropdown.addEventListener('mouseout', ()=>{
+//     icon_dropdown.style.backgroundColor = null;
+// });
+// topperIcons.forEach((topperIcon)=>{
+//     topperIcon.style.color = "white";
+//     topperIcon.addEventListener('mouseover', ()=>{
+//         topperIcon.style.backgroundColor = "var(--darkModeLight)";
+//     });
+//     topperIcon.addEventListener('mouseout', ()=>{
+//         topperIcon.style.backgroundColor = null;
+//     });
+// });
--- workarea/index.php
+++ workarea/index.php
@@ -6,25 +6,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hazlo Todo | Workarea</title>
     <link rel="stylesheet" href="../styles/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <script src="https://kit.fontawesome.com/1279a1c1cc.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="workarea_container">
-        <div class="workarea_topper">
-            <div class="avatar">
-                <div class="circle"></div>
-                <p class="userFullName">Elimihele God's Favour</p>
-            </div>
+    <div class="workarea_topper">
+        <div class="profile_fullname">
+            <div class="circle"></div>
+            <p class="user_fullname">Precious Okeypraise .C.</p>
         </div>
 
-        <div class="main_container">
-            <div class="workarea_nav">
+        <form class="search_activity">
+            <input type="search" name="search_activity_input" placeholder="Search your activities..." id="search_activity_input" class="search_activity_input">
+            <button type="submit">
+                <i class="fa fa-magnifying-glass"></i>
+            </button>
+        </form>
 
-            </div>
+        <div class="topper_icons">
+            <button class="switchMode topperIcons">
+                <i class="fa fa-sun"></i>
+            </button>
 
-            <div class="workarea_main">
+            <button class="notifications topperIcons">
+                <i class="fa fa-bell"></i>
+            </button>
 
+            <button class="addActivity topperIcons">
+                <i class="fa fa-plus"></i>
+            </button>
+
+            <div class="icon_dropdown">
+                <div class="user_circle">
+                    <i class="fa fa-user"></i>
+                </div>
+                <div class="drop_arrow">
+                    <i class="fa fa-angle-down"></i>
+                </div>
             </div>
         </div>
     </div>
+    <div class="workarea_container">
+        <div class="workarea_nav">
+            
+        </div>
+        <div class="workarea_main">
+
+        </div>
+    </div>
+
+    <script src="../scripts/workarea.js"></script>
 </body>
 </html>
