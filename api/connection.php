fa9a2c9f3749e0b74d4ef9d80bd4f04c4fd7f5d4
Praisecodes <cmancoder@gmail.com>
2022-07-15 02:19:20 +0100

    Feat: Sign Out (undergoing testing)

--- api/connection.php
+++ api/connection.php
@@ -2,15 +2,15 @@
     header("Content-Type: application/json");
     header("Access-Control-Allow-Origin: *");
 
-    $conn = new mysqli("localhost", "root", "", "hazlotododb");
+    // $conn = new mysqli("localhost", "root", "", "hazlotododb");
 
-    // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    // $cleardb_server = $cleardb_url["host"];
-    // $cleardb_username = $cleardb_url["user"];
-    // $cleardb_password = $cleardb_url["pass"];
-    // $cleardb_db = substr($cleardb_url["path"],1);
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
 
-    // $conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    $conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
     if($conn->connect_error){
         echo json_encode([
